f3b3f4f722fd034b30cc035d0fe07966c2ffb0e0
sazukeR <reinaldojcg1@gmail.com>
2023-05-27 15:33:48 -0500

    Add todo list with classes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import handleKeyDown from './components/handleKeyDown.js';
+// import handleKeyDown from './components/handleKeyDown.js';
+import TodoList from './components/todoListWithClasses.js';
 import './styles.css';
-import editTask from './components/editTask.js';
-import preChargedTask from './components/preChargedTask.js';
+// import editTask from './components/editTask.js';
+// import preChargedTask from './components/preChargedTask.js';
 
-preChargedTask();
-const taskInput = document.getElementById('taskInput');
-taskInput.addEventListener('keydown', (e) => handleKeyDown(e));
-document.addEventListener('click', (e) => editTask(e));
+// preChargedTask();
+// const taskInput = document.getElementById('taskInput');
+// taskInput.addEventListener('keydown', (e) => handleKeyDown(e));
+// document.addEventListener('click', (e) => editTask(e));
+
+const todoList = new TodoList();
+todoList.renderTasks();
